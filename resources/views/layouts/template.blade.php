a0a5e2d92e952cbbb284e6355e53e7b30281be09
Alfast456 <alfast456@gmail.com>
2025-05-14 16:03:39 +0700

    view dashboard

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/apple-icon.png') }}" />
+    <link rel="icon" type="image/png" href="{{ asset('assets/img/favicon.png') }}" />
+    <title>Soft UI Dashboard Tailwind</title>
+    <!--     Fonts and icons     -->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
+    <!-- Font Awesome Icons -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <!-- Nucleo Icons -->
+    <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <!-- Popper -->
+    <script src="https://unpkg.com/@popperjs/core@2"></script>
+    <!-- Main Styling -->
+    <link href="{{ asset('assets/css/perfect-scrollbar.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/soft-ui-dashboard-tailwind.css?v=1.0.5') }}" rel="stylesheet" />
+</head>
+<!-- Styles -->
+@wirechatStyles
+@vite('resources/js/app.js') {{-- Atau bootstrap.js jika kamu split --}}
+@livewireStyles
+
+<body class="m-0 font-sans antialiased font-normal text-base leading-default bg-gray-50 text-slate-500">
+    <!-- sidenav  -->
+    @include('layouts.sidenav')
+
+    <!-- end sidenav -->
+
+    <main class="ease-soft-in-out xl:ml-68.5 relative h-full max-h-screen rounded-xl transition-all duration-200">
+        <!-- Navbar -->
+        @include('layouts.navbar')
+
+        <!-- end Navbar -->
+
+        <!-- cards -->
+        @yield('content')
+        <!-- end cards -->
+    </main>
+
+    <!-- JavaScript -->
+    <!-- plugin for charts  -->
+    <script src="{{ asset('assets/js/plugins/chartjs.min.js') }}" async></script>
+    <!-- plugin for scrollbar  -->
+    <script src="{{ asset('assets/js/plugins/perfect-scrollbar.min.js') }}" async></script>
+    <!-- github button -->
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
+    <!-- main script file  -->
+    <script src="{{ asset('assets/js/soft-ui-dashboard-tailwind.js?v=1.0.5') }}" async></script>
+    @wirechatAssets
+    
+    
+    @livewireScripts 
+  </body>
+  </html>
